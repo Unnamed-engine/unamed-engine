813718df5f40abbe02368a91c40edebd69ffd4a3
Alan Ramirez <alan5142@hotmail.com>
2024-12-25 17:43:22 -0600

    feat: devtool format command feat: devtool tidy command

--- src/devtool/src/args.rs
+++ src/devtool/src/args.rs
@@ -1,7 +1,9 @@
 use crate::commands::build::BuildCommand;
 use crate::commands::clicommand::CliCommand;
 use crate::commands::configure::ConfigureCommand;
+use crate::commands::format::FormatCommand;
 use crate::commands::new_file::NewFileCommand;
+use crate::commands::tidy::TidyCommand;
 use clap::{Parser, Subcommand};
 use std::process::ExitCode;
 
@@ -18,6 +20,8 @@ pub enum Cmd {
     Configure(ConfigureCommand),
     Build(BuildCommand),
     NewFile(NewFileCommand),
+    Format(FormatCommand),
+    Tidy(TidyCommand),
 }
 
 impl DevtoolCliOptions {
@@ -26,6 +30,8 @@ impl DevtoolCliOptions {
             Cmd::Configure(config) => config.run(),
             Cmd::Build(build) => build.run(),
             Cmd::NewFile(new_file) => new_file.run(),
+            Cmd::Format(format) => format.run(),
+            Cmd::Tidy(tidy) => tidy.run(),
         }
     }
 }
--- src/devtool/src/commands/utils.rs
+++ src/devtool/src/commands/utils.rs
@@ -1,6 +1,7 @@
 use anyhow::anyhow;
 use regex::Regex;
-use std::process::Command;
+use std::process::{Command, Stdio};
+use tracing::error;
 
 /// Gets the installed macOS version
 pub fn cmake_version() -> anyhow::Result<String> {
@@ -26,4 +27,39 @@ pub fn git_username() -> anyhow::Result<String> {
     let user_name = String::from_utf8_lossy(&git_command.stdout);
 
     Ok(user_name.trim().to_string())
+}
+
+pub fn get_project_files(filter: Vec<&str>, only_modified: bool) -> anyhow::Result<Vec<String>> {
+    let mut command = Command::new("git");
+    command
+        .args(["ls-files"])
+        .args(filter)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
+
+    if only_modified {
+        command.arg("-m");
+    }
+
+    let output = command.output()?;
+    if !output.status.success() {
+        let error_message = "git ls-files -m failed";
+        error!("Output: {}", String::from_utf8_lossy(&output.stderr));
+        return Err(anyhow!(error_message));
+    }
+
+    let modified_files = String::from_utf8_lossy(&output.stdout)
+        .lines()
+        .map(|s| s.to_string())
+        .collect();
+
+    Ok(modified_files)
+}
+
+pub fn create_stdio(verbose: bool) -> (Stdio, Stdio) {
+    if verbose {
+        (Stdio::inherit(), Stdio::inherit())
+    } else {
+        (Stdio::piped(), Stdio::piped())
+    }
 }
